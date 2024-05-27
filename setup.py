ce5833426b9a8527deb816a10b0498516a08d3a0
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-05-27 12:54:08 -0400

    fixed setup file...including version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='generate_temp_table_sql',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -17,7 +17,7 @@ setup(
     description='A package to generate SQL statements from CSV files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/generate_temp_table_sql',
+    url='https://github.com/ryanwaldorf/generate_temp_table_sql',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
