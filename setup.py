bc9ed4c89595de14117306775b83434000524716
Ryan Waldorf <ryanwaldorf@Ryans-MacBook-Pro-2.local>
2024-05-27 12:56:34 -0400

    trying to fix versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='generate_temp_table_sql',
-    version='0.4',
+    version='0.4.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
